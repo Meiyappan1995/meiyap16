e7e3938bead6a2bf6a29e5f9e8b12039c1d3d7dd
Meiyappan1995 <meiyappanp95@gmail.com>
2019-03-30 10:51:20 +0530

    Create a base class on patrent class

---
+++ src/com/test/test1/Projectclass8AM.java
@@ -0,0 +1,17 @@
+package com.test.test1;
+
+public class Projectclass8AM {
+	public void empId(String name) {
+		System.out.println("Employee name is:"+"\t"+name);
+	}
+
+	public void empId(float sal, long phno) {
+		System.out.println("Employee personal details is:"+sal+"\t"+phno);
+
+	}
+	
+	public void empId(int regno,String project ) {
+		System.out.println("Company details is:"+regno+"\t"+"Project name is" +project);
+
+	}
+}
