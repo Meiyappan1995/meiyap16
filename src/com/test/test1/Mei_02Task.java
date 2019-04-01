62fa8c90b92bedc8ee0e9dcf8f3c3d37813e406d
Meiyappan1995 <meiyappanp95@gmail.com>
2019-04-01 15:21:35 +0530

    Second class added

---
+++ src/com/test/test1/Mei_02Task.java
@@ -0,0 +1,16 @@
+package com.test.test1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mei_02Task {
+public static void main(String[] args) {
+	List <Integer> li=new ArrayList <Integer>();
+	List <Integer> l1=new ArrayList <Integer>();
+	li.add(10);
+	li.add(20);
+		System.out.println(li);
+		System.out.println(l1);
+	}
+}
+
